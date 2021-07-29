522159305d9b7ec712e74d02840d30edf2fb635d
MAMATHALOGANATH <71583272+MAMATHALOGANATH@users.noreply.github.com>
2021-07-29 12:36:28 +0530

    Update staff.java

--- staff.java
+++ staff.java
@@ -1,3 +1,8 @@
+/*Design a superclass called Staff with details as StaffId, Name, Phone, Salary. Extend this 
+class by writing three subclasses namely Teaching (domain, publications), Technical
+(skills), and Contract (period). Write a Java program to read and display at least 3 staff
+objects of all three categories.*/
+
 package lab2;
 
 import java.util.Scanner; 
